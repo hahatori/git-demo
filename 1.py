3965c0c1cfb5ee4cae52277a713db63834affb96
Tori <hahatori@users.noreply.github.com>
2020-01-03 02:26:53 +0000

    change1

---
+++ 1.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import messagebox
+
+
+root = Tk()
+
+root.title("my window")
+root.geometry("500x300+100+200")
+
+btn01 = Button(root)
+btn01["text"] = "hit me"
+
+btn01.pack()
+
+def songhua(e):
+    messagebox.showinfo("Message", "you hit me")
+    print("give you flower")
+
+
+btn01.bind('<Button-1>', songhua)
+
+root.mainloop()
